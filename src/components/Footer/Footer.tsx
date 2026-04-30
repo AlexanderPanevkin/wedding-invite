15258d9eeee7c7716e735c9088ea6d4046673a30
AlexanderPanevkin <panevkin900@gmail.com>
2026-04-30 09:43:30 +0300

    added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,16 @@
+import stayedBefore from "../../assets/stayedBefore.png.webp";
+import './Footer.scss'; // Не забудьте импортировать стили!
+
 export const Footer = () => {
-    return
+    return (
+        <footer className="footer">
+            <div className="container">
+                <img
+                    src={stayedBefore}
+                    alt="Ждем с нетерпением"
+                    className="footer-img"
+                />
+            </div>
+        </footer>
+    )
 }
